66b6c1b5fd4ca2e5721a0b0ae19ae0aa7de47ae5
ronaldemanuel <emronald15el@gmail.com>
2021-11-09 13:16:49 -0300

    feat: implement store animal function on animal controller feat: implement create function for routing

--- app/Http/Controllers/Animal/AnimalController.php
+++ app/Http/Controllers/Animal/AnimalController.php
@@ -15,4 +15,44 @@ class AnimalController extends Controller
         return view('dashboard', ['animals' => $animals]);
     }
 
+    public function create()
+    {
+        return view('animals.create');
+    }
+
+    public function store(Request $request)
+    {
+        $animal = new Animal;
+
+        $animal->name = $request->name;
+        $animal->race = $request->race;
+        $animal->age = $request->age;
+
+        $user = auth()->user();
+        $animal->user_id = $user->id;
+
+        $animal->save();
+
+        return redirect('dashboard');
+    }
+
+    public function edit($id)
+    {
+        $user = auth()->user();
+
+        $animal = Animal::findOrFail($id);
+
+        if ($user->id != $animal->user->id) {
+            return redirect('dashboard');
+        }
+
+        return view('animals.edit', ['animal' => $animal]);
+    }
+
+    public function destroy($id)
+    {
+        Animal::findOrFail($id)->delete();
+
+        return redirect('dashboard');
+    }
 }
