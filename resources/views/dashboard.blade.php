570f5be22926ca7e7eaf3bbbf19e1405c3bcd8dd
ronaldemanuel <emronald15el@gmail.com>
2021-11-10 12:14:46 -0300

    feat: add edit button on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,13 +14,16 @@
                         <h3>
                             {{ $animal->name }}
                         </h3>
-                        <form action="/animals/{{ $animal->id }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <x-button class="ml-3 bg-red-700" type="submit">
-                                {{ __('Excluir') }}
-                            </x-button>
-                        </form>
+                        <div class="flex">
+                            <a href="{{ url("animals/$animal->id/edit") }}" class="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Editar</a>
+                            <form action="{{ url("animals/$animal->id") }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <x-button class="ml-3 bg-red-700" type="submit">
+                                    {{ __('Excluir') }}
+                                </x-button>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 @endforeach
